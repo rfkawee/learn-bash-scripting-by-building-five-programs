7ca82249163075ee3294a865eca8ef4e9db677c0
asumeringah 123 <ryfkialwi01@gmail.com>
2024-10-28 07:38:30 +0000

    Save progress: 990.1

--- .freeCodeCamp/test.sh
+++ .freeCodeCamp/test.sh
@@ -3,7 +3,7 @@
 
 echo -e "\n~~ Bingo Number Generator ~~\n" 
 
-NUMBER=$(( RANDOM % 15+46 ))
+NUMBER=$(( RANDOM % 15+61 ))
 
 TEXT="The next number is, "
 
@@ -19,4 +19,6 @@ then
 elif [[ $NUMBER -lt 61 ]]
 then
   echo $TEXT G:$NUMBER
+else 
+  echo $TEXT O:$NUMBER
 fi
